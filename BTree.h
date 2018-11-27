ac1b60a17889597a4147e783e1aaf0f96277ccd1
matteomejia <32267660+matteomejia@users.noreply.github.com>
2018-11-27 12:58:14 -0500

    v2

--- BTree.h
+++ BTree.h
@@ -6,9 +6,10 @@
 #define BTREE_BTREE_H
 
 #include "node.h"
+#include <iostream>
 
 class BTree {
-    node *root;
+    Node *root;
     int min_degree;
 public:
     BTree(int min_degree) {
@@ -16,46 +17,30 @@ public:
         this->min_degree = min_degree;
     }
 
-    void traverse()
-    {
-        if (root != nullptr){
+    void traverse() {
+        if(root != nullptr) {
             root->traverse();
         }
     }
 
-    node* search(int key)
-    {
+    Node *search(int key) {
         if(root == nullptr) {
             return nullptr;
         }else {
-            return root->search(key);
+            root->search(key);
         }
     }
 
     void insert(int key) {
-        if (root == nullptr) {
-            root = new node(min_degree, true);
+        Node *node = root;
+
+        if(root == nullptr) {
+            root = new Node(true, min_degree);
             root->keys[0] = key;
-            root->n = 1;
-        } else
-        if (root->n == 2*min_degree-1) {
-            node *s = new node(min_degree, false);
 
-            s->childs[0] = root;
-
-            s->split(0, root);
-
-            int i = 0;
-            if (s->keys[0] < key) {
-                i++;
-            }
-            s->childs[i]->insertNonFull(key);
-
-            root = s;
-        } else {
-            root->insertNonFull(key);
         }
     }
+
 };
 
 #endif //BTREE_BTREE_H
--- node.h
+++ node.h
@@ -7,112 +7,54 @@
 
 #include <iostream>
 
-class node {
+class Node {
 public:
-    node **childs; //array de hijos
-    int *keys;  //array de llaves
-    int min_degree; //grado minimo
-    int n;  //numero de llaves
-    bool isLeaf;    //booleano si es hoja
+    int *keys;
+    bool isLeaf;
+    int min_degree;
+    int nkeys;
+    Node **childs;
 public:
-    node(int min_degree, bool isLeaf) {
+    Node(bool isLeaf, int min_degree) {
         this->min_degree = min_degree;
         this->isLeaf = isLeaf;
+
         this->keys = new int[2 * min_degree - 1];
-        this->childs = new node * [2 * min_degree];
-        this->n = 0;
+        this->childs = new Node *[2 * min_degree];
+
+        this->nkeys = 0;
     }
 
     void traverse() {
-        int i;
-        for (i = 0; i < n; i++) {
-            if (!isLeaf) {
+        int i = 0
+        for(i = 0; i < nkeys; ++i) {
+            if(!isLeaf) {
                 childs[i]->traverse();
             }
             std::cout << " " << keys[i];
         }
 
-        if (!isLeaf) {
+        if(!isLeaf) {
             childs[i]->traverse();
         }
-    };
-
-    void insertNonFull(int key) {
-        int i = n-1;
-
-        if (isLeaf) {
-            while (i > -1 && keys[i] > key) {
-                keys[i + 1] = keys[i];
-                i--;
-            }
-            keys[i + 1] = key;
-            n = n + 1;
-        }
-        else {
-            while (i > -1 && keys[i] > key) {
-                i--;
-            }
-        }
-
-        if (childs[i+1]->n == 2*min_degree-1) {
-            split(i + 1, childs[i + 1]);
-
-            if (keys[i+1] < key) {
-                i++;
-            }
-        }
-        childs[i+1]->insertNonFull(key);
     }
 
-    void split(int i, node *y) {
-        node *x = new node(y->min_degree, y->isLeaf);
-        x->n = min_degree - 1;
-
-        for (int j = 0; j < min_degree-1; j++) {
-            x->keys[j] = y->keys[j + min_degree];
+    Node *search(int key) {
+        int count = 0;
+        while(count < n && key > keys[count]) {
+            count++;
         }
 
-        if (!(y->isLeaf)) {
-            for (int j = 0; j < min_degree; j++) {
-                x->childs[j] = y->childs[j + min_degree];
-            }
-        }
-
-        y->n = min_degree - 1;
-
-        for (int j = n; j >= i+1; j--) {
-            childs[j + 1] = childs[j];
-        }
-
-        childs[i + 1] = x;
-
-        for (int j = n-1; j >= i; j--) {
-            keys[j + 1] = keys[j];
-        }
-
-        keys[i] = y->keys[min_degree - 1];
-
-        n = n + 1;
-    }
-
-
-    node *search(int key){
-        int i = 0;
-        while (i < n && key > keys[i]) {
-            i++;
-        }
-
-        if (keys[i] == key) {
+        if(keys[count] == key) {
             return this;
         }
 
-        if (isLeaf) {
+        if(isLeaf) {
             return nullptr;
         }
 
-        return childs[i]->search(key);
+        return childs[count]->search(key);
     }
-
 };
 
 #endif //BTREE_NODE_H
